9f8a4f7852ec1463fdf4402b68a2f1d5bcefb06d
Yoga F. Syahputra <121499055+yogs14@users.noreply.github.com>
2024-10-20 20:11:08 +0700

    Add files via upload

---
+++ Pertemuan 8/Parser.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Parser {
+
+    private CommandWords commands;
+
+    public Parser() {
+        commands = new CommandWords();
+    }
+    
+    public Command getCommand() {
+        String inputLine = " ";
+        String word1;
+        String word2;
+    
+        System.out.println("> ");
+    
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            inputLine  = reader.readLine();
+        }
+        catch(java.io.IOException exc) {
+            System.out.println("There was an error during reading: " + exc.getMessage());
+        }
+    
+        StringTokenizer tokenizer = new StringTokenizer(inputLine);
+    
+        if (tokenizer.hasMoreTokens()) {
+            word1 = tokenizer.nextToken();
+        }
+        else {
+            word1 = null;
+        }
+        if (tokenizer.hasMoreTokens()) {
+            word2 = tokenizer.nextToken();
+        }
+        else {
+            word2 = null;
+        }
+    
+        if (commands.isCommand(word1)) {
+            return new Command(word1, word2);
+        }
+        else {
+            return new Command(null, word2);
+        }
+    }
+    
+}
